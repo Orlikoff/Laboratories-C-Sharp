cbc0177948be6f89125fe733280f9dedb3ad9568
Orlikoff <studyorlik@gmail.com>
2021-03-29 12:55:41 +0300

    Update of C#

---
+++ Lab 4/KeyLogger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)] static extern int MessageBox(int hWnd, string text, string caption, uint type);
+
+        static void Main(string[] args)
+        {
+            MessageBox(0, "Hello", "Title", 0);
+        }
+    }
+}
