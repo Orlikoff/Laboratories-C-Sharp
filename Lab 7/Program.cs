03df3107c157a74c5b04419c861f57f63c0552d9
Orlikoff <studyorlik@gmail.com>
2021-05-07 17:00:44 +0300

    Update of C#

--- Lab 7/Program.cs
+++ Lab 7/Program.cs
@@ -6,7 +6,19 @@ namespace Lab_7
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            List<RationalNumber> NUMBERS = new List<RationalNumber>();
+
+            RationalNumber m = 1.342f;
+            RationalNumber l = 2.564f;
+            RationalNumber p = -9.32f;
+            RationalNumber q = -2.789f;
+
+            NUMBERS.Add(m);
+            NUMBERS.Add(l);
+            NUMBERS.Add(p);
+            NUMBERS.Add(q);
+
             RationalNumber n1 = new RationalNumber(1, 7);
             RationalNumber n2 = new RationalNumber(2, 9);
             RationalNumber res = n2 / n1;
@@ -16,14 +28,8 @@ namespace Lab_7
             RationalNumber c = 9;
             RationalNumber d = 4.5f;
             RationalNumber t = 5.225f;
-            Console.WriteLine(d == t);
-            Console.WriteLine(t);
-            Console.WriteLine(r);
             RationalNumber f = RationalNumber.Parse("25/5");
             RationalNumber g = RationalNumber.Parse("5.000");
-            Console.WriteLine(f);
-            Console.WriteLine(g);
-            Console.WriteLine(g > f);
         }
     }
 }
